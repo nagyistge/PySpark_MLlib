747356bba24cc3fdb0fb20bee8d83d106d789925
Michael Li <lijingru931102@gmail.com>
2017-07-14 12:09:39 -0400

    commit 2
    basic pyspark dataframe manipulation

--- test1.py
+++ test1.py
@@ -4,6 +4,9 @@
 import pyspark
 import numpy as np
 import matplotlib.pyplot as plt
+from pyspark import SQLContext
+from pyspark import SparkContext
+sc = SparkContext()
 
 # import dataframe from s3
 filepath = 's3a://mdv-sandbox/michael/python_testing/data/raw'
@@ -51,6 +54,34 @@ df.groupby('token4').agg({'patient_date_of_birth': 'mean'}).show()
 # groupby and count number of entries
 df.groupby('patient_date_of_birth').count().show()
 
+# create sample dataframe 
+# 3 parameters: 
+#			1. withReplacement = True or False to select a observation with or without replacement.
+#			2. fraction = x, where x = .5 shows that we want to have 50% data in sample DataFrame.
+#			3. seed for reproduce the result
+train1 = df.sample(False, 0.3, 1) 
 
+# apply map functions on columns of dataframe
+# print first 5 elements of mapped year in date of birth
+df.select('patient_date_of_birth').map(lambda x: str(x)[:5]).take(5)
 
+# sort dataframe based on columns
+df.orderBy(df.patient_date_of_birth.desc()).show(5)
 
+# add new columns in dataframe
+# add a patient age column
+df.withColumn('patient_age', 2017 - df.patient_date_of_birth.str()[:5].astype(int)).select('patient_date_of_birth', 'patient_age').show(5)
+
+# drop columns in dataframe
+df.drop('token9').columns
+
+# find difference token4 between two dataframes (a super set and a subset)
+diff_df = df.select('token4').subtract(train1.select('token4'))
+diff_df.distinct().count()
+
+# apply SQL queries on a dataframe
+# 1. register dataframe as a table
+df.registerAsTable('df_table')
+
+# 2. apply SQL queries on table, result is a dataframe
+sqlContext.sql('select patient_age, count(token4) from df_table group by patient_age').show()
